640a522eb99936a8b143c15ea1c2cf83ef6630b3
Primate Lab <100426365+primatelab@users.noreply.github.com>
2023-05-18 11:50:02 +0100

    Update tablebox.sh

--- tablebox.sh
+++ tablebox.sh
@@ -1,2 +1,2 @@
 # PrimateLab's magical tableboxing oneliner! https://github.com/primatelab/tablebox. Use in your own bash scripts!
-sed 's/^/,/;s/$/,/;s/,/,│,/g'|column -t -s,|(d(){ sed "s/[^│]/─/g;s/^│/$1/g;s/│$/$2/g;s/│/$3/g";};read a;echo "$a"|d ┌ ┐ ┬;echo -E "$a";echo "$a"|d ├ ┤ ┼;while read c;do echo -E "$c"; done; echo "$a"|d └ ┘ ┴)
+eval $(base64 -d <<< H4sIAAAAAAACA12MvwrCMBCHd5/iCIG2kHKo480+hVgoSWiF2oCpOFhBnB0cMjg4Ojo6+jR9Es8WER0+7nd/vvPWQOQxQ4XkUQ5FoerCUWERtdpVm1UNaQOpV21s4mQHnh3hcZ7x0QK7cMCCpXeHctxnjhLl5JNRTrEQtKe1zQ3kZHXpQMhctAa6cGLOzH2Yp7N+9Xd0ZW7Mk7blsrLQf9JkHHwlLQiMqy3BrxuYC/NIRi++rQlQ7wAAAA== | gzip -d)
